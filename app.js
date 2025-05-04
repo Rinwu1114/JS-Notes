f4e2ada0ace7ad15bcac88079331712f23da4a84
Rinwu1114 <Rinwu1114@gmail.com>
2025-05-04 02:17:06 -0500

    added nav bar, data types and variables, and equality section

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('asdfsadf')
+console.log('Hello World'[10])
+console.log('Hello World'.length)
+
+let str = 'Hello World'
+console.log(str[str.length - 1])
+
+
+// console.log('hold ctrl + / to comment')
+
+let fullName = 'Rin Wu';
+
+/**
+ * PRACTICE
+ * 
+ * Convert Celsius to Fahrenheit
+ * 
+ *  F = C x 1.8 +32 
+ */
+
+let celsius = "10";
+let fahrenheit = celsius * 1.8 + 32;
+console.log(fahrenheit);
