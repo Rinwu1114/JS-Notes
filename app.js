2163aae32944f5d5368c6cf5b99ab33021beeb59
Rinwu1114 <Rinwu1114@gmail.com>
2025-05-07 03:30:43 -0500

    added conditionals section and examples in js file

--- app.js
+++ app.js
@@ -20,3 +20,30 @@ let fullName = 'Rin Wu';
 let celsius = "10";
 let fahrenheit = celsius * 1.8 + 32;
 console.log(fahrenheit);
+
+/**
+ * Conditionals practice
+ */
+
+let cash = 50
+let price = 40
+let remainder = cash-price
+let isStoreOpen = true
+let affordable = cash >= price
+let shopping = affordable && isStoreOpen ? `Give receipt` : `Don't give receipt`
+console.log(shopping)
+
+
+/**
+if (cash > price) {
+    console.log(`you paid extra - here's ${remainder} dollars change` )
+}
+else if (cash === price) {
+    console.log(`You paid the exact amount, have a dice day!`)
+}
+else{
+    console.log(`Not enough money - you still owe ${remainder*-1} dollars`)
+}
+ */
+
+
